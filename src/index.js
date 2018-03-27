f1d2d6623e69f098b9cf25aeb0dbfa55067e30b8
husterlihuijuan <781413809@qq.com>
2018-03-27 11:26:02 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ButtonAppBar from './test.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+ReactDOM.render(
+  <ButtonAppBar />,
+  document.getElementById('root')
+);
